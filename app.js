699c3f37e12ec5647e5bc7623ba98e83be1f2c69
Marius Zalieckas <marius.zalieckas@gmail.com>
2022-05-25 19:22:46 +0300

    classwork js random,find min/max

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('hello world');
+
+var foo = 'bar';
+
+console.log(foo);
+
+let a = 'hello';
+
+console.log(a);
+
+const b = 'world';
+
+console.log(b);
+
+let x = 5,
+    y = 4;
+
+    console.log(x + y);
+    console.log(x - y);
+    console.log(x * y);
+    console.log(x / y);
+
+let name = 'Marius';
+let city = 'Alytus';
+
+console.log('Hello. My name is ' + name + '. Im from ' + city );
+console.log(`Hello. My name is ${name}. Im from ${city}. ` );
+
