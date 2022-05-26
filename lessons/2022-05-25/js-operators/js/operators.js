967dfc0e9f71506af699ca3bdd96c548c3354ae7
Marius Zalieckas <marius.zalieckas@gmail.com>
2022-05-26 18:09:07 +0300

    lessom 22-05-25

---
+++ lessons/2022-05-25/js-operators/js/operators.js
@@ -0,0 +1,34 @@
+let str = 'hello world';
+let otherStr = str;
+
+console.log(str, otherStr);
+
+
+console.log(3 + 1);
+console.log(3 - 1);
+console.log(3 / 1);
+console.log(3 * 1);
+console.log(12/5, 12 % 5);
+
+
+let num = 4;
+
+num++;
+num = num + 1;
+console.log(num);
+
+num = 2;
+num += 5; //num = num + 5;
+console.log(num);
+
+num = 5;
+num -= 2; //num = num - 2;
+console.log(num);
+
+console.log('\\\\\\\\\\\\\\');
+console.log(5 > 2);
+console.log(5 < 2);
+console.log(5 == 2);
+console.log(5 == 5);
+console.log(5 == '5', 5 === '5');
+
