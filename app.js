39ca55f6fa2e8b8a06a659f36c5da62b8ba48984
Marius Zalieckas <marius.zalieckas@gmail.com>
2022-06-09 21:59:10 +0300

    classwork 06-09

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-let arr = [11, -2, 34, 45, 19, 6];
-
-function getMaxSubSum() {
-
-    let r = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] > 0) {
-            r = r + arr[i];
-        }
-    }
-    return r;
-}
+    const btn = document.querySelector('#button')
+    const enterText = document.getElementById("enter-text");
+    const display = document.querySelector("#display");
+    button.addEventListener("click", function (event) {
+       display.value = enterText.value;
+    });
 
 
-console.log(getMaxSubSum(arr));
+// console.log(btn);
+
+// if (btn) {
+//     console.log(btn);
+
+//     btn.addEventListener('click', function(event) {
+//         console.log('anonymous function', event);
+//     });
+
+//     btn.addEventListener('click', (event) => {
+//         console.log('anonymous arrow function', event);
+//     });
+
+//     function clickEventHandler(event) {
+//         console.log('clickEventHandler', event);
+//     }
+
+//     btn.addEventListener('click', clickEventHandler);
+
+//     const clickEventHandlerAsVariable = function(event) {
+//         console.log('clickEventHandlerAsVariable', event);
+//     }
+
+//     btn.addEventListener('click', clickEventHandlerAsVariable);
+// }
+
+// document.querySelector('button:last-of-type')?.addEventListener('click', clickEventHandler);
+// document.querySelector('span')?.addEventListener('click', clickEventHandler);
+
+
