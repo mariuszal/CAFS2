1a88b56e98c8b13e27ebd360a684b3ef5a094b37
Marius Zalieckas <marius.zalieckas@gmail.com>
2022-06-13 18:02:07 +0300

    homework 06-09

--- app.js
+++ app.js
@@ -5,34 +5,3 @@
        display.value = enterText.value;
     });
 
-
-// console.log(btn);
-
-// if (btn) {
-//     console.log(btn);
-
-//     btn.addEventListener('click', function(event) {
-//         console.log('anonymous function', event);
-//     });
-
-//     btn.addEventListener('click', (event) => {
-//         console.log('anonymous arrow function', event);
-//     });
-
-//     function clickEventHandler(event) {
-//         console.log('clickEventHandler', event);
-//     }
-
-//     btn.addEventListener('click', clickEventHandler);
-
-//     const clickEventHandlerAsVariable = function(event) {
-//         console.log('clickEventHandlerAsVariable', event);
-//     }
-
-//     btn.addEventListener('click', clickEventHandlerAsVariable);
-// }
-
-// document.querySelector('button:last-of-type')?.addEventListener('click', clickEventHandler);
-// document.querySelector('span')?.addEventListener('click', clickEventHandler);
-
-
